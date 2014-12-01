352fd1ee4994624912b1cdbcb08f518b15724c6d
Kwangju Kim <kimk3@miamioh.edu>
2014-12-01 16:07:02 -0500

    .

--- ProjectAppStuff/src/java/helpers/DBHandling.java
+++ ProjectAppStuff/src/java/helpers/DBHandling.java
@@ -28,9 +28,25 @@ public class DBHandling {
     
     public static boolean Login(User u) throws Exception {
         stmt = initializer();
-        String statement = String.format("SELECT USERNAME, PASSWORD FROM USER WHERE USERNAME = '%s' AND PASSWORD = '%s'", u.getName(), u.getPassword());
+	String statement;
+        if (u.isAdmin()) {
+            statement = String.format("SELECT USERNAME, PASSWORD FROM ADMIN WHERE USERNAME = '%s' AND PASSWORD = '%s'", u.getName(), u.getPassword());
+        } else if (u.isModerator()) {
+            statement = String.format("SELECT USERNAME, PASSWORD FROM MODERATOR WHERE USERNAME = '%s' AND PASSWORD = '%s'", u.getName(), u.getPassword());
+        } else {
+            statement = String.format("SELECT USERNAME, PASSWORD FROM USER WHERE USERNAME = '%s' AND PASSWORD = '%s'", u.getName(), u.getPassword());
+        }
         ResultSet rs = stmt.executeQuery(statement);
+        int count = 0;
         while (rs.next()) {
+            ++count;
+        }
+        
+        if (count > 1) {
+            throw new IllegalStateException();
+        }
+        if (count == 1) {
+            rs.first();
             String foundID = rs.getString("USERNAME");
             String foundPW = rs.getString("PASSWORD");
             if (u.getName().equals(foundID)) {
@@ -44,7 +60,15 @@ public class DBHandling {
     
     public static boolean SignUp(User u) throws Exception {
         stmt = initializer();
-        String statement = String.format("INSERT INTO USER " + "VALUES ('%s', '%s');", u.getName(), u.getPassword());
+	String statement;
+        if (u.isAdmin()) {
+            statement = String.format("INSERT INTO ADMIN " + "VALUES ('%s', '%s');", u.getName(), u.getPassword());
+        } else if (u.isModerator()) {
+            statement = String.format("INSERT INTO MODERATOR " + "VALUES ('%s', '%s');", u.getName(), u.getPassword());
+        } else {
+            statement = String.format("INSERT INTO USER " + "VALUES ('%s', '%s');", u.getName(), u.getPassword());
+        }
+        
         try {
             stmt.executeUpdate(statement);
         } catch (SQLException e) {
